53d57f78f04093b8e8c2ec2885432beb231b5fdc
Harshalszz <477harshz@gmail.com>
2025-06-19 22:44:21 +0530

    RotateList Day13

--- DSAProblems/src/ProblemsDay_13/RotateList.java
+++ DSAProblems/src/ProblemsDay_13/RotateList.java
@@ -6,26 +6,30 @@ public class RotateList {
 //https://leetcode.com/problems/rotate-list/description/
     public static ListNode rotateList(ListNode head){
         int k = 2;
-
+        //find the len and tail of the list
         int l = 1;
         ListNode n = head;
         while(n.next != null){
             l++;
             n = n.next;
         }
+        //point the tail to head
+
         n.next = head;
 
         k= k%l;
-
+        // if k is multple of length of list , same head will be return
         if(k == 0){
             n.next = null;
             return head;
         }
+
+        //move the temp to l-k
         ListNode temp = head;
         for(int i=1; i<l-k;i++){
             temp = temp.next;
         }
-
+        //now new head will be temp.next;
         head = temp.next;
         temp.next = null;
 
