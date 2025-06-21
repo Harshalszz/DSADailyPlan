bae0343f987369a6ce9e41781b54e737c3a1fe04
Harshalszz <477harshz@gmail.com>
2025-06-21 18:49:54 +0530

    MiddleoftheLinkedList Day15

---
+++ DSAProblems/src/ProblmesDay_15/MiddleoftheLinkedList.java
@@ -0,0 +1,22 @@
+package ProblmesDay_15;
+
+import ProblemsDay2.ListNode;
+
+public class MiddleoftheLinkedList {
+//https://leetcode.com/problems/middle-of-the-linked-list/description/
+    public static ListNode middleoftheLinkedList(ListNode head){
+
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next!= null){
+            slow =slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
