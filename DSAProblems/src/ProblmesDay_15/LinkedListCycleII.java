c62b262b4d6befcd635145636c3dcce17dd62d79
Harshalszz <477harshz@gmail.com>
2025-06-21 18:58:31 +0530

    LinkedListCycleII Day15

---
+++ DSAProblems/src/ProblmesDay_15/LinkedListCycleII.java
@@ -0,0 +1,37 @@
+package ProblmesDay_15;
+
+import ProblemsDay2.ListNode;
+
+import java.util.List;
+
+public class LinkedListCycleII {
+//https://leetcode.com/problems/linked-list-cycle-ii/description/
+    public static ListNode linkedListCycleII(ListNode head){
+
+        if(head == null || head.next == null){
+            return null;
+        }
+
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if(fast == null || fast.next == null) return null;
+
+        fast = head;
+
+        while(fast != slow){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
