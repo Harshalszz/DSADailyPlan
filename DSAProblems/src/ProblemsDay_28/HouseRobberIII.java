8f3d3d811b648f840b2ddfa8b350b84a58ee817d
Harshalszz <477harshz@gmail.com>
2025-07-04 23:41:05 +0530

    HouseRobber3 Day28

---
+++ DSAProblems/src/ProblemsDay_28/HouseRobberIII.java
@@ -0,0 +1,28 @@
+package ProblemsDay_28;
+
+import ProblemsDay_25.TreeNode;
+
+public class HouseRobberIII {
+//https://leetcode.com/problems/house-robber-iii/
+    public int rob(TreeNode root) {
+        if(root == null)
+            return 0;
+        int[] result = helper(root);
+        return Math.max(result[0], result[1]);
+    }
+    public int[] helper(TreeNode root){
+        if(root == null){
+            int[] result = {0, 0};
+            return result;
+        }
+        int[] result = new int[2];
+        int[] left = helper(root.left);
+        int[] right = helper (root.right);
+        result[0] = root.val + left[1] + right[1];
+        result[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return result;
+    }
+    public static void main(String[] args) {
+
+    }
+}
