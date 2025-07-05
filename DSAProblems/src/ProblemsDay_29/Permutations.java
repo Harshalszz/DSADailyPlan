29d2a802675cbabc43a29225f73d4c98fa05a219
Harshalszz <477harshz@gmail.com>
2025-07-05 21:14:44 +0530

    Permutations Day29

---
+++ DSAProblems/src/ProblemsDay_29/Permutations.java
@@ -0,0 +1,42 @@
+package ProblemsDay_29;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+
+    //https://leetcode.com/problems/permutations/description/
+    private static void backtracking(List<List<Integer>> resList, List<Integer> tempList, int[] nums) {
+
+        if(tempList.size() == nums.length){
+            resList.add(new ArrayList(tempList));
+            return;
+        }
+
+        for(int n : nums){
+            if(tempList.contains(n)){
+                continue;
+            }
+
+            tempList.add(n);
+
+            backtracking(resList,tempList,nums);
+
+            tempList.remove(tempList.size()-1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+
+        List<List<Integer>> resList = new ArrayList<>();
+
+        backtracking(resList, new ArrayList<>(), nums);
+
+
+        System.out.println(resList);
+    }
+
+
+}
