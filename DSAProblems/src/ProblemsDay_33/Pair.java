760b8bb1aa332f982ac4e60e364284ec9e7e8b9d
Harshalszz <477harshz@gmail.com>
2025-07-09 22:39:36 +0530

    MaximumWidthBinaryTree Day33

---
+++ DSAProblems/src/ProblemsDay_33/Pair.java
@@ -0,0 +1,17 @@
+package ProblemsDay_33;
+
+public class Pair<U, V> {
+    public final U first;
+    public final V second;
+
+    public Pair(U first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public V getValue() {
+    }
+
+    public U getKey() {
+    }
+}
