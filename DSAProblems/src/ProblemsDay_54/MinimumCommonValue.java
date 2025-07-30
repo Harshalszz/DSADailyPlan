d9e85212d1f83ce7d306cf120cb944c83cce3729
Harshalszz <477harshz@gmail.com>
2025-07-30 14:48:22 +0530

    MinimumCommonValue Day54

---
+++ DSAProblems/src/ProblemsDay_54/MinimumCommonValue.java
@@ -0,0 +1,26 @@
+package ProblemsDay_54;
+
+public class MinimumCommonValue {
+
+    public int getCommon(int[] nums1, int[] nums2) {
+
+        int i=0 ,j=0;
+
+        while(j<nums2.length && i<nums1.length){
+            if(nums1[i] == nums2[j]){
+                return nums1[i];
+            }else if(nums1[i] < nums2[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
