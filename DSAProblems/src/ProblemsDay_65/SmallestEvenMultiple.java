a5848a68f009b8722e291baf1ac91c437bb3f370
Harshalszz <477harshz@gmail.com>
2025-08-10 13:13:28 +0530

    SmallestEvenMultiple Day65

---
+++ DSAProblems/src/ProblemsDay_65/SmallestEvenMultiple.java
@@ -0,0 +1,14 @@
+package ProblemsDay_65;
+
+public class SmallestEvenMultiple {
+    //https://leetcode.com/problems/smallest-even-multiple/
+    public int smallestEvenMultiple(int n) {
+
+        return n%2==0?n:n*2;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
