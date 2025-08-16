44c1031f94ee32e32ff3459bdc17b1adfded052e
Harshalszz <477harshz@gmail.com>
2025-08-16 20:15:06 +0530

    LargestNumber Day71

---
+++ DSAProblems/src/ProblemsDay_71/LargestNumber.java
@@ -0,0 +1,28 @@
+package ProblemsDay_71;
+
+import java.util.Arrays;
+
+public class LargestNumber {
+
+    //https://leetcode.com/problems/largest-number/
+
+    public String largestNumber(int[] nums) {
+        String[] array =  new String[nums.length];
+        for(int i=0; i<nums.length; i++){
+            array[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(array,(a, b)-> (b+a).compareTo(a+b));
+        if(array[0].equals("0")){
+            return "0";
+        }
+        StringBuilder largest = new StringBuilder();
+        for(int i=0; i<array.length; i++){
+            largest.append(array[i]);
+        }
+        return largest.toString();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
