d316c7375b529b491b754f32069fd0c2e0aaadef
Harshalszz <477harshz@gmail.com>
2025-08-17 12:53:13 +0530

    CanPlaceFlowers Day72

---
+++ DSAProblems/src/ProblemsDay_72/CanPlaceFlowers.java
@@ -0,0 +1,35 @@
+package ProblemsDay_72;
+
+public class CanPlaceFlowers {
+    //https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        int count = 0;
+
+        for(int i=0; i<flowerbed.length; i++){
+
+            if(flowerbed[i] == 0){
+
+                boolean left = (i==0) || (flowerbed[i-1] == 0);
+
+                boolean right = (i==flowerbed.length - 1) || (flowerbed[i+1] == 0);
+
+                if(left && right){
+                    flowerbed[i] = 1;
+                    count++;
+                }
+
+            }
+
+
+        }
+
+        return count>=n;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
