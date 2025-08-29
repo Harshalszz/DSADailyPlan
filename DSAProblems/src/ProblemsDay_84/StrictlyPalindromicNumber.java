6d9282884ef728750bc4978feaf69191a742a919
Harshalszz <477harshz@gmail.com>
2025-08-29 22:39:57 +0530

    StrictlyPalindromicNumber Day84

---
+++ DSAProblems/src/ProblemsDay_84/StrictlyPalindromicNumber.java
@@ -0,0 +1,13 @@
+package ProblemsDay_84;
+
+public class StrictlyPalindromicNumber {
+    //https://leetcode.com/problems/strictly-palindromic-number/
+
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
