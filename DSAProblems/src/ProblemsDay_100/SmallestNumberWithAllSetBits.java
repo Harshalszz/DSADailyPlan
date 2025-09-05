8b43d9c514d3d3ff6049eba8886a5c62a404ef76
Harshalszz <477harshz@gmail.com>
2025-09-05 20:37:34 +0530

    SmallestNumberWithAllSetBits Day91

---
+++ DSAProblems/src/ProblemsDay_100/SmallestNumberWithAllSetBits.java
@@ -0,0 +1,31 @@
+package ProblemsDay_100;
+
+public class SmallestNumberWithAllSetBits {
+
+    //https://leetcode.com/problems/smallest-number-with-all-set-bits/
+
+    public int smallestNumber(int n) {
+
+        // int res = 1;
+
+        // while(res < n){
+        //     res = res*2 + 1;
+        // }
+
+        // return res;
+
+        int res = n;
+
+        while((res & (res +1) )!=0){//checking if all bits are set bits
+
+            res++;
+        }
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
