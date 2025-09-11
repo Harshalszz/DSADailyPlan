c7b29578f9b0db86ec82df946211e93af95bf970
Harshalszz <477harshz@gmail.com>
2025-09-11 22:30:57 +0530

    MergeSortedArray Day97

---
+++ DSAProblems/src/ProblemsDay_97/MergeSortedArray.java
@@ -0,0 +1,33 @@
+package ProblemsDay_97;
+
+public class MergeSortedArray {
+
+    //https://leetcode.com/problems/merge-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
