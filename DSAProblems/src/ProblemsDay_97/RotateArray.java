9546fac036ef92e7bbfac476c2bd8f6f8416e22b
Harshalszz <477harshz@gmail.com>
2025-09-11 22:30:13 +0530

    RotateArray Day97

---
+++ DSAProblems/src/ProblemsDay_97/RotateArray.java
@@ -0,0 +1,28 @@
+package ProblemsDay_97;
+
+public class RotateArray {
+
+    //https://leetcode.com/problems/rotate-array/?envType=study-plan-v2&envId=top-interview-150
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
