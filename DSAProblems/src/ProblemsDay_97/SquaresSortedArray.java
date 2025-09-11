77a7a7ca5040444315df7c9cebf36b76faec9bdb
Harshalszz <477harshz@gmail.com>
2025-09-11 22:31:40 +0530

    SquaresSortedArray Day97

---
+++ DSAProblems/src/ProblemsDay_97/SquaresSortedArray.java
@@ -0,0 +1,26 @@
+package ProblemsDay_97;
+
+public class SquaresSortedArray {
+    //https://leetcode.com/problems/squares-of-a-sorted-array/
+
+    public int[] sortedSquares(int[] nums) {
+        int[] res = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                res[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                res[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
