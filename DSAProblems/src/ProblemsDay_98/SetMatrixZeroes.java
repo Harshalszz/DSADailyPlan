460a2475d54d181f53d6fdefb4d7fce505ed20fa
Harshalszz <477harshz@gmail.com>
2025-09-12 19:58:27 +0530

    SetMatrixZeroes Day98

---
+++ DSAProblems/src/ProblemsDay_98/SetMatrixZeroes.java
@@ -0,0 +1,55 @@
+package ProblemsDay_98;
+
+public class SetMatrixZeroes {
+
+    //https://leetcode.com/problems/set-matrix-zeroes/?envType=study-plan-v2&envId=top-interview-150
+
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        // Use a variable to track if the first column needs to be zeroed
+        int col0 = 1;
+
+        // First pass: Use the first row and column as markers
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(matrix[i][j] == 0){
+                    // Mark the row
+                    matrix[i][0] = 0;
+                    // Mark the column. Handle the first column separately.
+                    if(j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        // Second pass: Use markers to set the inner matrix to zero
+        for(int i = 1; i < n ; i++){
+            for(int j = 1 ; j < m ; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Final pass: Zero out the first row and/or column if needed
+        if(matrix[0][0] == 0) {
+            for(int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if(col0 == 0) {
+            for(int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
