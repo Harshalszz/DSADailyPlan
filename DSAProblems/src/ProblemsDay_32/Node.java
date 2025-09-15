8ee7d5951f75387618eef0e4169e971bccc10a16
Harshalszz <477harshz@gmail.com>
2025-09-15 21:51:44 +0530

    CopyListwithRandomPointer Day101

--- DSAProblems/src/ProblemsDay_32/Node.java
+++ DSAProblems/src/ProblemsDay_32/Node.java
@@ -6,16 +6,19 @@ public class Node {
     public Node right;
     public Node next;
 
+    public Node random;
+
     public Node() {}
 
     public Node(int _val) {
         val = _val;
     }
 
-    public Node(int _val, Node _left, Node _right, Node _next) {
+    public Node(int _val, Node _left, Node _right, Node _next,  Node _random) {
         val = _val;
         left = _left;
         right = _right;
         next = _next;
+        random = _random;
     }
 }
