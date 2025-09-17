5a34ff3b9985b7b4c6b78afee8a3bac52953f160
Harshalszz <477harshz@gmail.com>
2025-09-17 22:29:22 +0530

    RearrangeArrayElementsbySign Day103

---
+++ DSAProblems/src/ProblmesDay_103/RearrangeArrayElementsbySign.java
@@ -0,0 +1,32 @@
+package ProblmesDay_103;
+
+public class RearrangeArrayElementsbySign {
+
+    //https://leetcode.com/problems/rearrange-array-elements-by-sign/
+
+    public int[] rearrangeArray(int[] nums) {
+
+        int[] res = new int[nums.length];
+
+        int posIndex = 0;
+        int negIndex = 1;
+
+        for(int i=0; i<nums.length; i++){
+
+            if(nums[i] < 0){
+                res[negIndex] = nums[i];
+                negIndex += 2;
+            }else{
+                res[posIndex] = nums[i];
+                posIndex +=2;
+            }
+        }
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
