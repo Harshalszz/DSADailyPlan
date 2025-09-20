b8444c23f0033f3d5f916534c7c72cb8f3d5621f
Harshalszz <477harshz@gmail.com>
2025-09-20 18:35:16 +0530

    CourseSchedule Day106

--- DSAProblems/src/ProblemsDay_106/CourseSchedule.java
+++ DSAProblems/src/ProblemsDay_106/CourseSchedule.java
@@ -2,9 +2,29 @@ package ProblemsDay_106;
 
 import java.util.ArrayList;
 
-public class DetectCycleInDirectedGraph {
+public class CourseSchedule {
 
-    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj){
+    //https://leetcode.com/problems/course-schedule/?envType=study-plan-v2&envId=top-interview-150
+
+    // used dfs to detect cycle
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        int V = numCourses;
+        int E = prerequisites.length;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
+        for(int i = 0; i < E; i++){
+            int node1 = prerequisites[i][0];
+            int node2 = prerequisites[i][1];
+            adj.get(node2).add(node1);
+        }
+
+        return !detectCycle(V,adj);
+
+    }
+
+    public boolean detectCycle(int V, ArrayList<ArrayList<Integer>> adj){
 
         int[] visited = new int[V];
         int[] pathVis = new int[V];
@@ -29,7 +49,7 @@ public class DetectCycleInDirectedGraph {
         for(int it : adj.get(i)){
 
             if(visited[it] == 0){
-                if(dfsCycle(it , visited,pathVis,adj) == true){
+                if(dfsCycle(it , visited,pathVis,adj)){
                     return true;
                 }
             }else if(pathVis[it] == 1){
