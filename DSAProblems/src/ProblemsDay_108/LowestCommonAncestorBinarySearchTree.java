c92d5aada1bdd792e1dfad3f41c25dde506a61b2
Harshalszz <477harshz@gmail.com>
2025-09-22 21:48:19 +0530

    LowestCommonAncestorBinarySearchTree Day108

---
+++ DSAProblems/src/ProblemsDay_108/LowestCommonAncestorBinarySearchTree.java
@@ -0,0 +1,37 @@
+package ProblemsDay_108;
+
+import ProblemsDay_25.TreeNode;
+
+public class LowestCommonAncestorBinarySearchTree {
+
+    //first by using dfs will find the node
+    //once any of the two given node is foud
+    //we will simple return that node
+    // also if both the left and right nod eis not ull then will reun the root itself
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(root == null || root == p || root == q){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p,q);
+        TreeNode right = lowestCommonAncestor(root.right, p,q);
+
+
+        if(left == null){
+            return right;
+        }else if(right == null){
+            return left;
+        }else{
+
+            //if both tyhe left and right are not null, then we have find the LCA of p and q
+            return root;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
