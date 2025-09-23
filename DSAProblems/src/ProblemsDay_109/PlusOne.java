4c3ed625599517df2df6c0e8a1fdbd9499e3eece
Harshalszz <477harshz@gmail.com>
2025-09-23 21:56:15 +0530

    PlusOne Day109

---
+++ DSAProblems/src/ProblemsDay_109/PlusOne.java
@@ -0,0 +1,23 @@
+package ProblemsDay_109;
+
+public class PlusOne {
+    //https://leetcode.com/problems/plus-one/?envType=study-plan-v2&envId=top-interview-150
+
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
