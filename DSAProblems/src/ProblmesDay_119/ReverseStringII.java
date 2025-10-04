0eb816b032e6054ffd745a52fbd7eae1db6fa416
Harshalszz <477harshz@gmail.com>
2025-10-04 21:54:14 +0530

    ReverseStringII Day119

---
+++ DSAProblems/src/ProblmesDay_119/ReverseStringII.java
@@ -0,0 +1,36 @@
+package ProblmesDay_119;
+
+public class ReverseStringII {
+
+    //https://leetcode.com/problems/reverse-string-ii/?envType=problem-list-v2&envId=two-pointers
+
+    public void reverse(char[] arr , int i , int j) {
+        while(i <= j) {
+            char t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+
+            i++;
+            j--;
+        }
+    }
+
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = s.length();
+
+        int i = 0;
+        while(i < n) {
+            int j = Math.min(i + k - 1 , n - 1);
+            reverse(arr , i , j);
+
+            i += 2 * k;
+        }
+
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
