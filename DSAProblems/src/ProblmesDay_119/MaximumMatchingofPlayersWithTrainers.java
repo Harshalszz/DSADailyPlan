38da5cba5ac04e4dba55c81313ff49e787395d73
Harshalszz <477harshz@gmail.com>
2025-10-04 21:50:04 +0530

    MaximumMatchingofPlayersWithTrainers Day119

---
+++ DSAProblems/src/ProblmesDay_119/MaximumMatchingofPlayersWithTrainers.java
@@ -0,0 +1,32 @@
+package ProblmesDay_119;
+
+import java.util.Arrays;
+
+public class MaximumMatchingofPlayersWithTrainers {
+    //https://leetcode.com/problems/maximum-matching-of-players-with-trainers/
+
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+
+        int res = 0;
+        int i=0, j=0;
+
+        while(i<players.length && j < trainers.length){
+
+            if(players[i] <= trainers[j]){
+                res++;
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+
+        return res;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
