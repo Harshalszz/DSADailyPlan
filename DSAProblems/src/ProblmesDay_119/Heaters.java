5a531b55bbc2d8199a2bd96e0a4bd56167e4eb76
Harshalszz <477harshz@gmail.com>
2025-10-04 21:48:26 +0530

    Heaters Day119

---
+++ DSAProblems/src/ProblmesDay_119/Heaters.java
@@ -0,0 +1,47 @@
+package ProblmesDay_119;
+
+import java.util.Arrays;
+
+public class Heaters {
+
+    //https://leetcode.com/problems/heaters/?envType=problem-list-v2&envId=two-pointers
+
+    public static int findRadius(int[] houses, int[] heaters) {
+
+        int n = houses.length;
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int l = 0;
+            int r = heaters.length-1;
+            int house = houses[i];
+            int minDist = Integer.MAX_VALUE;
+
+            while(l<=r) {
+
+                int mid = l + (r-l)/2;
+
+                int heater = heaters[mid];
+                int diff = Math.abs(heater - house);
+                minDist = Math.min(minDist, diff);
+
+                if(heater < house) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+
+            }
+
+            ans = Math.max(ans, minDist);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
