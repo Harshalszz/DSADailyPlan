e8ed6c9386d78e4afc9a89a21205eac74017262f
Harshalszz <477harshz@gmail.com>
2025-10-05 19:15:44 +0530

    DesignHashSet Day120

---
+++ DSAProblems/src/ProblemsDay_120/DesignHashSet.java
@@ -0,0 +1,29 @@
+package ProblemsDay_120;
+
+public class DesignHashSet {
+//https://leetcode.com/problems/design-hashset/?envType=problem-list-v2&envId=linked-list
+    class MyHashSet {
+
+        private boolean[] storage;
+
+        public MyHashSet() {
+            storage = new boolean[1_000_001]; // Initialize array for keys in range [0, 1000000]
+        }
+
+        public void add(int key) {
+            storage[key] = true; // Mark key as present
+        }
+
+        public void remove(int key) {
+            storage[key] = false; // Mark key as absent
+        }
+
+        public boolean contains(int key) {
+            return storage[key]; // Check if key is present
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
