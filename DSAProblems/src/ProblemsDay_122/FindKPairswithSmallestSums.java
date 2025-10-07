fbf2552bae9b5f1f1fd2fedfb9a7400b6c6660d8
Harshalszz <477harshz@gmail.com>
2025-10-07 20:16:31 +0530

    FindKPairswithSmallestSums Day122

---
+++ DSAProblems/src/ProblemsDay_122/FindKPairswithSmallestSums.java
@@ -0,0 +1,38 @@
+package ProblemsDay_122;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class FindKPairswithSmallestSums {
+//https://leetcode.com/problems/find-k-pairs-with-smallest-sums/?envType=problem-list-v2&envId=array
+
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        for(int x : nums1) {
+            pq.offer(new int[] {x + nums2[0], 0});
+        }
+
+        while(k > 0 && !pq.isEmpty()) {
+            int[] pair = pq.poll();
+            int sum = pair[0];
+            int pos = pair[1];
+
+            list.add(List.of(sum - nums2[pos], nums2[pos]));
+
+            if(pos + 1 < nums2.length) {
+                pq.offer(new int[] {sum - nums2[pos] + nums2[pos + 1], pos + 1});
+            }
+            k--;
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
