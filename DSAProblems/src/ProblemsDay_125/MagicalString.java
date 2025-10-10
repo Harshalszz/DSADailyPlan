91253e88b19ad5c3e2bfa6ae73fbc9922a404d95
Harshalszz <477harshz@gmail.com>
2025-10-10 20:02:18 +0530

    MagicalString Day125

---
+++ DSAProblems/src/ProblemsDay_125/MagicalString.java
@@ -0,0 +1,28 @@
+package ProblemsDay_125;
+
+public class MagicalString {
+
+    //https://leetcode.com/problems/magical-string/?envType=problem-list-v2&envId=two-pointers
+
+    public int magicalString(int n) {
+        StringBuilder sb= new StringBuilder("122");
+        int w=2;
+        char r='1';
+        while(sb.length()<n){
+            String repeat1= String.valueOf(r).repeat(sb.charAt(w)-'0');
+            sb.append(repeat1);
+            w++;
+            if(r=='1') r='2';
+            else r='1';
+        }
+        int cnt=0;
+        for(int i=0;i<n;++i){
+            if(sb.charAt(i)=='1') cnt++;
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
