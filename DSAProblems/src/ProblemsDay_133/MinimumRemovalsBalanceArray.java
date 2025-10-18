6130cc2b82eef50993ff77086655f79e2483ca32
Harshalszz <477harshz@gmail.com>
2025-10-18 20:03:23 +0530

    MinimumRemovalsBalanceArray Day133

---
+++ DSAProblems/src/ProblemsDay_133/MinimumRemovalsBalanceArray.java
@@ -0,0 +1,26 @@
+package ProblemsDay_133;
+
+import java.util.Arrays;
+
+public class MinimumRemovalsBalanceArray {
+
+//https://leetcode.com/problems/minimum-removals-to-balance-array/
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums); // Sort array to maintain order
+        int n = nums.length;
+        int maxSize = 0;
+        int left = 0;
+
+        // Use sliding window to find longest valid subarray
+        for (int right = 0; right < n; right++) {
+            // While condition is violated, shrink window from left
+            while (nums[right] > (long) k * nums[left]) left++;
+            maxSize = Math.max(maxSize, right - left + 1);
+        }
+        // Return number of elements to remove
+        return n - maxSize;
+    }
+    public static void main(String[] args) {
+
+    }
+}
