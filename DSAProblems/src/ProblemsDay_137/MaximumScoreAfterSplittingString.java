f9321496ef62ef0ba97c043d690fd5b4207ad6d2
Harshalszz <477harshz@gmail.com>
2025-10-23 22:16:47 +0530

    MaximumScoreAfterSplittingString Day137

---
+++ DSAProblems/src/ProblemsDay_137/MaximumScoreAfterSplittingString.java
@@ -0,0 +1,33 @@
+package ProblemsDay_137;
+
+public class MaximumScoreAfterSplittingString {
+
+//https://leetcode.com/problems/maximum-score-after-splitting-a-string/?envType=problem-list-v2&envId=prefix-sum
+    public int maxScore(String inputStr) {
+        int totalOnes = 0;
+        int zerosCount = 0;
+        int onesCount = 0;
+        int bestScore = Integer.MIN_VALUE;
+
+        // Count total ones in the string
+        for (char ch : inputStr.toCharArray()) {
+            if (ch == '1') totalOnes++;
+        }
+
+        // Traverse the string and calculate scores
+        for (int i = 0; i < inputStr.length() - 1; i++) {
+            if (inputStr.charAt(i) == '0') zerosCount++;
+            else onesCount++;
+
+            // Calculate score
+            int currentScore = zerosCount + (totalOnes - onesCount);
+            bestScore = Math.max(bestScore, currentScore);
+
+        }
+
+        return bestScore;
+    }
+    public static void main(String[] args) {
+
+    }
+}
