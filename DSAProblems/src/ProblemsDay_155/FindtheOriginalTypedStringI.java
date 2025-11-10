6a5cb7d9ea9db056688376614480b072e5a48234
Harshalszz <477harshz@gmail.com>
2025-11-10 22:02:12 +0530

    FindtheOriginalTypedStringI Day155

---
+++ DSAProblems/src/ProblemsDay_155/FindtheOriginalTypedStringI.java
@@ -0,0 +1,26 @@
+package ProblemsDay_155;
+
+public class FindtheOriginalTypedStringI {
+
+    //https://leetcode.com/problems/find-the-original-typed-string-i/
+    public int possibleStringCount(String word) {
+        int ans = 1;
+        int count = 1;
+        char last = word.charAt(0);
+
+        for (int i = 1; i < word.length(); i++) {
+            if (word.charAt(i) != last) {
+                ans += count - 1;
+                count = 1;
+                last = word.charAt(i);
+            } else {
+                count++;
+            }
+        }
+        ans += count - 1;
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
+}
