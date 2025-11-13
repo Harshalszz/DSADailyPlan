2b6ab97164894c8f160ef6c4fc0d5c424173b17d
Harshalszz <477harshz@gmail.com>
2025-11-13 21:08:03 +0530

    RotateFunction Day158

---
+++ DSAProblems/src/ProblemsDay_158/RotateFunction.java
@@ -0,0 +1,28 @@
+package ProblemsDay_158;
+
+public class RotateFunction {
+
+    //https://leetcode.com/problems/rotate-function/?envType=problem-list-v2&envId=array
+    public int maxRotateFunction(int[] nums) {
+        int n=nums.length;
+        int res=Integer.MIN_VALUE;
+        int sum=0;
+        int sum2=0;
+        for(int i=0;i<n;i++){
+            sum+=(i*nums[i]);
+            sum2+=nums[i];
+        }
+        int t=n-1;
+        for(int i=0;i<n;i++){
+            int temp=sum2-nums[t-i];
+            int temp2=sum+temp-(t*nums[t-i]);
+            res=Math.max(temp2,res);
+            sum=temp2;
+
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+
+    }
+}
