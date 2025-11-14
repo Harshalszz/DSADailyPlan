5198ba801034987fe11ca874d70b131308694840
Harshalszz <477harshz@gmail.com>
2025-11-14 19:52:19 +0530

    BeautifulArrangement Day159

---
+++ DSAProblems/src/ProblemsDay_159/BeautifulArrangement.java
@@ -0,0 +1,33 @@
+package ProblemsDay_159;
+
+public class BeautifulArrangement {
+
+    //https://leetcode.com/problems/beautiful-arrangement/?envType=problem-list-v2&envId=array
+    public int countArrangement(int n) {
+        return backtrack(n, n, new Integer[1 << (n + 1)], 0);
+    }
+
+    private int backtrack(int n, int index, Integer[] cache, int cacheindex)
+    {
+        if(index == 0)
+            return 1;
+
+        int result = 0;
+        if(cache[cacheindex] != null)
+            return cache[cacheindex];
+
+        for(int i = n; i > 0; i--)
+        {
+            if((cacheindex & (1 << i)) == 0 &&  (i%(index) == 0 || (index)%i == 0))
+            {
+                result += backtrack(n, index - 1, cache, cacheindex | 1 << i);
+            }
+        }
+
+        cache[cacheindex] = result;
+        return result;
+    }
+    public static void main(String[] args) {
+
+    }
+}
