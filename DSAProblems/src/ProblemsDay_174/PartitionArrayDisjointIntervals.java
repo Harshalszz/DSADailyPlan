38863635996a95279ed2b00f78b2eeba174f03da
Harshalszz <477harshz@gmail.com>
2025-11-29 21:29:54 +0530

    PartitionArrayDisjointIntervals Day174

---
+++ DSAProblems/src/ProblemsDay_174/PartitionArrayDisjointIntervals.java
@@ -0,0 +1,24 @@
+package ProblemsDay_174;
+//https://leetcode.com/problems/partition-array-into-disjoint-intervals/
+public class PartitionArrayDisjointIntervals {
+    public int partitionDisjoint(int[] nums) {
+
+        int ans=1;
+        int currMax=nums[0];
+        int maxCanBe=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<currMax){
+                ans=i+1;
+                currMax=maxCanBe;
+            }
+            else{
+                maxCanBe=Math.max(maxCanBe,nums[i]);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
