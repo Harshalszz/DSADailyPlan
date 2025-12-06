769b4d39c82c3721443c4646f4de3ae0b4795a5f
Harshalszz <477harshz@gmail.com>
2025-12-06 20:21:22 +0530

    pass

--- DSAProblems/src/ProblemsDay_181/PassthePillow.java
+++ DSAProblems/src/ProblemsDay_181/PassthePillow.java
@@ -1,4 +1,20 @@
 package ProblemsDay_181;
 
 public class PassthePillow {
+//https://leetcode.com/problems/pass-the-pillow/
+    public int passThePillow(int n, int time) {
+        int completed = time/(n-1);
+        int remaining = time%(n-1);
+        int ans = 0;
+        if(completed%2!=0){
+            ans = n - remaining;
+        }else{
+            ans = remaining + 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
