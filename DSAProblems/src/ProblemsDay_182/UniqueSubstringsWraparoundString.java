3adbde6c0b8dbd2851f971e75258e87de9e83fb0
Harshalszz <477harshz@gmail.com>
2025-12-07 18:01:33 +0530

    UniqueSubstringsWraparoundString Day182

---
+++ DSAProblems/src/ProblemsDay_182/UniqueSubstringsWraparoundString.java
@@ -0,0 +1,28 @@
+package ProblemsDay_182;
+
+public class UniqueSubstringsWraparoundString {
+//https://leetcode.com/problems/unique-substrings-in-wraparound-string/?envType=problem-list-v2&envId=string
+    public int findSubstringInWraproundString(String s){
+        int dp[] = new int[s.length()];
+        dp[s.length() - 1] = 1;
+        int maxArray[] = new int[26];
+        maxArray[s.charAt(s.length()-1) - 'a'] = 1;
+        for(int i = s.length() - 2 ; i >= 0 ; i--){
+            if(s.charAt(i) == s.charAt(i+1) - 1 || s.charAt(i) == 'z' && s.charAt(i+1) == 'a'){
+                dp[i] = 1 + dp[i+1];
+            }else{
+                dp[i] = 1;
+            }
+            int key = s.charAt(i) - 'a';
+            maxArray[key] = Math.max(maxArray[key],dp[i]);
+        }
+        int res = 0;
+        for(int i=0;i<26;i++){
+            res = res + maxArray[i];
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+
+    }
+}
