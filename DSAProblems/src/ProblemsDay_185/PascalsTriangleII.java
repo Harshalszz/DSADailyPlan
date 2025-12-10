d2590f8f9e5529bab9301d269351829a7d6dd713
Harshalszz <477harshz@gmail.com>
2025-12-10 21:04:24 +0530

    PascalsTriangleII Day185

---
+++ DSAProblems/src/ProblemsDay_185/PascalsTriangleII.java
@@ -0,0 +1,24 @@
+package ProblemsDay_185;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangleII {
+
+//https://leetcode.com/problems/pascals-triangle-ii/?envType=problem-list-v2&envId=array
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> lst = new ArrayList<>();
+        int first = 1;
+        lst.add(first);
+        long res = 1;
+        for(int col=1; col<=rowIndex; col++){
+            res = res * (rowIndex - col + 1);
+            res = res / col;
+            lst.add((int)res);
+        }
+        return lst;
+    }
+    public static void main(String[] args) {
+
+    }
+}
