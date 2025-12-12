5de1a2350897bf2ae95abdc7470792fd1c087b64
Harshalszz <477harshz@gmail.com>
2025-12-12 21:18:07 +0530

    MinimumCosttoSetCookingTime Day187

---
+++ DSAProblems/src/ProblemsDay_187/MinimumCosttoSetCookingTime.java
@@ -0,0 +1,36 @@
+package ProblemsDay_187;
+
+public class MinimumCosttoSetCookingTime {
+
+    //https://leetcode.com/problems/minimum-cost-to-set-cooking-time/
+    public int minCostSetTime(int startAt, int moveCost, int pushCost, int targetSeconds) {
+        int m = targetSeconds / 60;
+        int s = targetSeconds % 60;
+        int one = find(startAt, moveCost, pushCost, m, s);
+        int two = find(startAt, moveCost, pushCost, m - 1, s + 60);
+        if (s >= 40 || m == 0) return one;
+        if (m >  99) return two;
+        return Math.min(one, two);
+    }
+
+    private int find(int i, int c, int p, int m, int s){
+        int ans = 0;
+        int[] data = new int[]{0, i}; //prev key sum, prev key
+        ans += each(c, p, m / 10, data);
+        ans += each(c, p, m % 10, data);
+        ans += each(c, p, s / 10, data);
+        ans += each(c, p, s % 10, data);
+        return ans;
+    }
+
+    private int each(int c, int p, int key, int[] data){
+        data[0] += key;
+        if (data[0] == 0) return 0;
+        int ans = (data[1] == key? p : p + c);
+        data[1] = key;
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
+}
