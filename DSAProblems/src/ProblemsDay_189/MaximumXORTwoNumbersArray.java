14bb385725bd822c1027b5b20a68ef76cb0ae9c3
Harshalszz <477harshz@gmail.com>
2025-12-14 13:53:55 +0530

    MaximumXORTwoNumbersArray Day189

---
+++ DSAProblems/src/ProblemsDay_189/MaximumXORTwoNumbersArray.java
@@ -0,0 +1,53 @@
+package ProblemsDay_189;
+
+public class MaximumXORTwoNumbersArray {
+//https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/?envType=problem-list-v2&envId=array
+    public int findMaximumXOR(final int[] nums) {
+        TrieNode root = new TrieNode();
+
+        for(final int num : nums) {
+            TrieNode curr = root;
+            for(int i = 30; i >= 0; i--) {
+                final int bit = (num >> i) & 1;
+
+                if(curr.children[bit] == null)
+                    curr.children[bit] = new TrieNode();
+
+                curr = curr.children[bit];
+            }
+        }
+
+        int max = 0;
+
+        for(final int num : nums) {
+            TrieNode curr = root;
+            int currSum = 0;
+
+            for(int i = 30; i >= 0; --i) {
+                int bit = 1 - ((num >> i) & 1);
+
+                if(curr.children[bit] == null) {
+                    curr = curr.children[1 - bit];
+                } else {
+                    currSum |= 1 << i;
+                    curr = curr.children[bit];
+                }
+            }
+
+            max = Math.max(max, currSum);
+        }
+
+        return max;
+    }
+
+    private class TrieNode {
+        public final TrieNode[] children;
+
+        public TrieNode() {
+            this.children = new TrieNode[2];
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
