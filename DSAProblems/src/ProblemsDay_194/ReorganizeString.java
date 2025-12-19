ff447f45cb03300e7c50c94c6d125d2c80ced33a
Harshalszz <477harshz@gmail.com>
2025-12-19 20:58:33 +0530

    ReorganizeString Day194

---
+++ DSAProblems/src/ProblemsDay_194/ReorganizeString.java
@@ -0,0 +1,39 @@
+package ProblemsDay_194;
+
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class ReorganizeString {
+//https://leetcode.com/problems/reorganize-string/?envType=problem-list-v2&envId=string
+    public String reorganizeString(String s) {
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> freqMap.get(b) - freqMap.get(a));
+        maxHeap.addAll(freqMap.keySet());
+
+        if (freqMap.get(maxHeap.peek()) > (s.length() + 1) / 2) {
+            return "";
+        }
+
+        StringBuilder res = new StringBuilder();
+        char[] result = new char[s.length()];
+        int i = 0;
+        while (!maxHeap.isEmpty()) {
+            char c = maxHeap.poll();
+            for (int j = 0; j < freqMap.get(c); j++) {
+                if (i >= s.length()) i = 1;
+                result[i] = c;
+                i += 2;
+            }
+        }
+
+        return new String(result);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
