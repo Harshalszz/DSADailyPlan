bc2043295af63e8d84fb12426cdf1d83424ac19b
Harshalszz <477harshz@gmail.com>
2025-12-26 21:47:01 +0530

    Triangle Day201

---
+++ DSAProblems/src/ProblemsDay_201/Triangle.java
@@ -0,0 +1,27 @@
+package ProblemsDay_201;
+
+import java.util.List;
+
+public class Triangle {
+//https://leetcode.com/problems/triangle/?envType=problem-list-v2&envId=array
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int row = triangle.size();
+        int[] memo = new int[row];
+
+        for (int i = 0; i < row; i++) {
+            memo[i] = triangle.get(row - 1).get(i);
+        }
+
+        for (int r = row-2; r >= 0; r--) {
+            for (int c = 0; c <= r; c++) {
+                memo[c] = Math.min(memo[c], memo[c+1]) + triangle.get(r).get(c);
+            }
+        }
+
+        return memo[0];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
