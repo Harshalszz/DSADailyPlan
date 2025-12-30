f981833c4d46086c5b16ea14a2c78999460a7409
Harshalszz <477harshz@gmail.com>
2025-12-30 21:11:22 +0530

    AlphabetBoardPath Day205

---
+++ DSAProblems/src/ProblemsDay_205/AlphabetBoardPath.java
@@ -0,0 +1,36 @@
+package ProblemsDay_205;
+
+public class AlphabetBoardPath {
+//https://leetcode.com/problems/alphabet-board-path/?envType=problem-list-v2&envId=string
+    public String alphabetBoardPath(String target) {
+
+        StringBuilder sb = new StringBuilder();
+
+        int sr = 0;
+        int sc = 0;
+
+        for (char ch : target.toCharArray()) {
+
+            int cr = (ch - 'a') / 5;
+            int cc = (ch - 'a') % 5;
+
+            sb.append("U".repeat(Math.max(0, sr - cr)));
+            sb.append("L".repeat(Math.max(0, sc - cc)));
+            sb.append("D".repeat(Math.max(0, cr - sr)));
+            sb.append("R".repeat(Math.max(0, cc - sc)));
+
+            sb.append("!");
+
+            sr = cr;
+            sc = cc;
+
+        }
+
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
