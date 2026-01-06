f36a1f10c2bb0974bee10d7cd2b448c125366c21
Harshalszz <477harshz@gmail.com>
2026-01-06 21:06:30 +0530

    BinaryTreesWithFactors Day212

---
+++ DSAProblems/src/ProblemsDay_212/BinaryTreesWithFactors.java
@@ -0,0 +1,37 @@
+package ProblemsDay_212;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class BinaryTreesWithFactors {
+
+    //https://leetcode.com/problems/binary-trees-with-factors/?envType=problem-list-v2&envId=array
+    int mod = (int)1e9+7;
+    public int numFactoredBinaryTrees(int[] arr) {
+        Arrays.sort(arr);
+        int ans =0;
+        HashMap<Integer, Long> map = new HashMap<>();
+
+        for(int x:arr)
+        {
+            long ways =1;
+            int max = (int)Math.sqrt(x);
+
+            for(int j = 0,left=arr[0];left<=max;left=arr[++j])
+            {
+                if(x%left!=0)   continue;
+                int right =x/left;
+                if(map.containsKey(right))
+                {
+                    ways = (ways+map.get(left)*map.get(right)*(left==right?1:2))%mod;
+                }
+            }
+            map.put(x,ways);
+            ans=(int)(ans+ways)%mod;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
+}
