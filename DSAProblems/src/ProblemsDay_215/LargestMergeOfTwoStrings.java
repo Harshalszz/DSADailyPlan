97ed8f7d7e36b091470c83e9769265a1824deb88
Harshalszz <477harshz@gmail.com>
2026-01-09 21:25:01 +0530

    LargestMergeOfTwoStrings Day215

---
+++ DSAProblems/src/ProblemsDay_215/LargestMergeOfTwoStrings.java
@@ -0,0 +1,43 @@
+package ProblemsDay_215;
+
+public class LargestMergeOfTwoStrings {
+
+    //https://leetcode.com/problems/largest-merge-of-two-strings/?envType=problem-list-v2&envId=two-pointers
+    public String largestMerge(String word1, String word2) {
+        char[] s1 = word1.toCharArray();
+        char[] s2 = word2.toCharArray();
+        int i = 0, j = 0, k = 0, n = s1.length, m = s2.length;
+        char[] str = new char[m + n];
+
+        while (i < n && j < m) {
+            if (s1[i] > s2[j])
+                str[k++] = s1[i++];
+            else if (s1[i] < s2[j])
+                str[k++] = s2[j++];
+            else {
+                int a = i, b = j;
+                while (a < n && b < m && s1[a] == s2[b]) {
+                    a++;
+                    b++;
+                }
+                if (a < n && b < m && s1[a] > s2[b]) {
+                    str[k++] = s1[i++];
+                } else if (b < m) {
+                    str[k++] = s2[j++];
+                } else {
+                    str[k++] = s1[i++];
+                }
+            }
+        }
+
+        while (i < n)
+            str[k++] = s1[i++];
+        while (j < m)
+            str[k++] = s2[j++];
+
+        return new String(str);
+    }
+    public static void main(String[] args) {
+
+    }
+}
