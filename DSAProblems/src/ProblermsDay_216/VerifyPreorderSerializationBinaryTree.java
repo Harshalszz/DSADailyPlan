cc71e8ad492378a6fc8404de20e17aa3fe7071aa
Harshalszz <477harshz@gmail.com>
2026-01-10 18:56:32 +0530

    VerifyPreorderSerializationBinaryTree Day216

---
+++ DSAProblems/src/ProblermsDay_216/VerifyPreorderSerializationBinaryTree.java
@@ -0,0 +1,23 @@
+package ProblermsDay_216;
+
+public class VerifyPreorderSerializationBinaryTree {
+
+    //https://leetcode.com/problems/verify-preorder-serialization-of-a-binary-tree/?envType=problem-list-v2&envId=binary-tree
+    public boolean isValidSerialization(String preorder) {
+        String[] nodes = preorder.split(",");
+        int diff = 1;
+        for(String nn : nodes){
+            diff--;
+            if(diff < 0){
+                return false;
+            }
+            if(!nn.equals("#")){
+                diff += 2;
+            }
+        }
+        return diff == 0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
