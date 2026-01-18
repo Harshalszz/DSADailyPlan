553bc0c3453dcd00d412279ece79638e6de73aa8
Harshalszz <477harshz@gmail.com>
2026-01-18 19:01:29 +0530

    MaximumProductLengthTwoPalindromicSubsequences Day224

---
+++ DSAProblems/src/ProblemsDay_224/MaximumProductLengthTwoPalindromicSubsequences.java
@@ -0,0 +1,33 @@
+package ProblemsDay_224;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaximumProductLengthTwoPalindromicSubsequences {
+
+    //https://leetcode.com/problems/maximum-product-of-the-length-of-two-palindromic-subsequences/?envType=problem-list-v2&envId=backtracking
+    Map<String,Integer> map;
+    public int maxProduct(String s) {
+        map = new HashMap<>();
+        return dfs("","",s,0);
+    }
+    private int dfs(String cur1,String cur2,String s,int idx){
+        if(idx==s.length()) return (isPalindrome(cur1)&&isPalindrome(cur2)) ? cur1.length()*cur2.length():0 ;
+        String key = cur1+","+cur2+","+idx;
+        if(map.containsKey(key)) return map.get(key);
+        map.put(key,Math.max (
+                dfs(cur1,cur2,s,idx+1),
+                Math.max(
+                        dfs(cur1+s.charAt(idx),cur2,s,idx+1),
+                        dfs(cur1,cur2+s.charAt(idx),s,idx+1)
+                )
+        ));
+        return map.get(key);
+    }
+    private boolean isPalindrome(String s){
+        return s.equals(new StringBuilder(s).reverse().toString());
+    }
+    public static void main(String[] args) {
+
+    }
+}
