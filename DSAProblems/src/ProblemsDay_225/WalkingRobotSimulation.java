70d037d38450fbda3036ced7e528c074e975a396
Harshalszz <477harshz@gmail.com>
2026-01-19 21:28:39 +0530

    WalkingRobotSimulation Day225

---
+++ DSAProblems/src/ProblemsDay_225/WalkingRobotSimulation.java
@@ -0,0 +1,43 @@
+package ProblemsDay_225;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WalkingRobotSimulation {
+//https://leetcode.com/problems/walking-robot-simulation/?envType=problem-list-v2&envId=array
+
+    public int robotSim(int[] commands, int[][] obstacles) {
+        Set<String> obstacleSet = new HashSet<>();
+        for (int[] obstacle : obstacles) {
+            obstacleSet.add(obstacle[0] + "," + obstacle[1]);
+        }
+
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // North, East, South, West
+        int x = 0, y = 0, direction = 0, maxDistance = 0;
+
+        for (int command : commands) {
+            if (command == -1) {
+                direction = (direction + 1) % 4;
+            } else if (command == -2) {
+                direction = (direction + 3) % 4;
+            } else {
+                for (int i = 0; i < command; i++) {
+                    int newX = x + directions[direction][0];
+                    int newY = y + directions[direction][1];
+                    if (!obstacleSet.contains(newX + "," + newY)) {
+                        x = newX;
+                        y = newY;
+                        maxDistance = Math.max(maxDistance, x * x + y * y);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return maxDistance;
+    }
+    public static void main(String[] args) {
+
+    }
+}
