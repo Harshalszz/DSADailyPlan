7d78a87aa9e36b922e3bf4797aced9c48d67f2ff
Harshalszz <477harshz@gmail.com>
2026-01-19 21:29:42 +0530

    ScoreAfterFlippingMatrix Day225

---
+++ DSAProblems/src/ProblemsDay_225/ScoreAfterFlippingMatrix.java
@@ -0,0 +1,27 @@
+package ProblemsDay_225;
+
+public class ScoreAfterFlippingMatrix {
+//https://leetcode.com/problems/score-after-flipping-matrix/?envType=problem-list-v2&envId=array
+    public int matrixScore(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int res = (1 << (m - 1)) * n;
+
+        for (int j = 1; j < m; ++j) {
+            int val = 1 << (m - 1 - j);
+            int set = 0;
+
+            for (int i = 0; i < n; ++i) {
+                if (grid[i][j] == grid[i][0]) {
+                    set++;
+                }
+            }
+
+            res += Math.max(set, n - set) * val;
+        }
+
+        return res;
+    }
+    public static void main(String[] args) {
+
+    }
+}
