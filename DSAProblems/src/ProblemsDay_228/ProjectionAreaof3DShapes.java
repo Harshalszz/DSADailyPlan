aa751be451f8be92747556ae4e663a21a63d5daa
Harshalszz <477harshz@gmail.com>
2026-01-22 20:30:47 +0530

    ProjectionAreaof3DShapes Day228

--- DSAProblems/src/ProblemsDay_228/ProjectionAreaof3DShapes.java
+++ DSAProblems/src/ProblemsDay_228/ProjectionAreaof3DShapes.java
@@ -2,7 +2,7 @@ package ProblemsDay_228;
 
 public class ProjectionAreaof3DShapes {
 
-    //https://leetcode.com/problems/projection-area-of-3d-shapes/?envType=problem-list-v2&envId=array
+    // https://leetcode.com/problems/projection-area-of-3d-shapes/?envType=problem-list-v2&envId=array
     public int projectionArea(int[][] grid) {
         int xy = 0, row[] = new int[grid.length], col[] = new int[grid[0].length];
         for (int i = 0; i < grid.length; i++)
