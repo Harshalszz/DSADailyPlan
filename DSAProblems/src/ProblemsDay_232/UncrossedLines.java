2dfd4dac8a7cc48a2095a84e74e70e42a5873884
Harshalszz <477harshz@gmail.com>
2026-01-26 18:23:23 +0530

    UncrossedLines Day232

---
+++ DSAProblems/src/ProblemsDay_232/UncrossedLines.java
@@ -0,0 +1,26 @@
+package ProblemsDay_232;
+
+public class UncrossedLines {
+
+    //https://leetcode.com/problems/uncrossed-lines/?envType=problem-list-v2&envId=array
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int[] dp = new int[nums2.length+1];
+
+        for(int i = 1; i <= nums1.length; i++) {
+            int[] dpRow = new int[nums2.length+1];
+            for(int j = 1; j <= nums2.length; j++) {
+                if(nums1[i-1] == nums2[j-1]) {
+                    dpRow[j] = dp[j-1] + 1;
+                } else {
+                    dpRow[j] = Math.max(dp[j], dpRow[j-1]);
+                }
+            }
+            dp = dpRow;
+        }
+
+        return dp[nums2.length];
+    }
+    public static void main(String[] args) {
+
+    }
+}
