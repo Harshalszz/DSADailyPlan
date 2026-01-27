d5599cf0431c0f54c8895f58654211438f5285e3
Harshalszz <477harshz@gmail.com>
2026-01-27 22:19:01 +0530

    ContainsDuplicate Day233

---
+++ DSAProblems/src/ProblemsDay_233/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+package ProblemsDay_233;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        return Arrays.stream(nums).distinct().count() < nums.length;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
