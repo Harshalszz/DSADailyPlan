9e15f758b13544b454178d33c8407f171043a8f7
Harshalszz <477harshz@gmail.com>
2026-01-28 21:30:59 +0530

    ValidPalindrome Day234

---
+++ DSAProblems/src/ProblemsDay_234/ValidPalindrome.java
@@ -0,0 +1,28 @@
+package ProblemsDay_234;
+
+public class ValidPalindrome {
+
+    //https://leetcode.com/problems/valid-palindrome/
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int left = 0, right = s.length() - 1;
+
+        while (left <= right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
