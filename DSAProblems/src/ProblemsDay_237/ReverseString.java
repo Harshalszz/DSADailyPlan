2104f1d5c75465f1442c6fc291702ca2e39ed2da
Harshalszz <477harshz@gmail.com>
2026-01-31 20:57:28 +0530

    ReverseString Day237

---
+++ DSAProblems/src/ProblemsDay_237/ReverseString.java
@@ -0,0 +1,19 @@
+package ProblemsDay_237;
+
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+
+        int left = 0, right = s.length-1;
+
+        while(left<=right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+
+    }
+}
