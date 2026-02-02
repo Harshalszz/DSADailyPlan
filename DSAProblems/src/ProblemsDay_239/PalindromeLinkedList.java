f69ab81291757d1be8916156a6d9cf3b1b9e0d4c
Harshalszz <477harshz@gmail.com>
2026-02-02 21:46:18 +0530

    PalindromeLinkedList Day239

---
+++ DSAProblems/src/ProblemsDay_239/PalindromeLinkedList.java
@@ -0,0 +1,43 @@
+package ProblemsDay_239;
+
+import ProblemsDay2.ListNode;
+
+public class PalindromeLinkedList {
+
+    public boolean isPalindrome(ListNode head) {
+
+        ListNode slow = head , fast =head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+
+        while(slow != null){
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+
+        ListNode first = head, second = prev;
+
+        while(second !=null){
+
+            if(first.val != second.val){
+                return false;
+            }
+
+            first = first.next;
+            second = second.next;
+        }
+
+
+        return true;
+    }
+    public static void main(String[] args) {
+
+    }
+}
