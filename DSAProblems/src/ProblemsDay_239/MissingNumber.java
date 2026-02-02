1961cca5211a90f25f64ac6a91347afb58333e0f
Harshalszz <477harshz@gmail.com>
2026-02-02 21:47:59 +0530

    MissingNumber Day239

---
+++ DSAProblems/src/ProblemsDay_239/MissingNumber.java
@@ -0,0 +1,21 @@
+package ProblemsDay_239;
+
+public class MissingNumber {
+//https://leetcode.com/problems/missing-number/
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+        int sum = (n *(n+1))/2;
+
+        int sumArr = 0;
+        for(int ni : nums){
+            sumArr +=ni;
+        }
+
+        return sum - sumArr ;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
