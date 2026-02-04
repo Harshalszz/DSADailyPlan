0c325b6588e4fa7358d6bd19236e539b675fd5a2
Harshalszz <477harshz@gmail.com>
2026-02-04 20:23:56 +0530

    LargestSubstringBetweenTwoEqual Day241

---
+++ DSAProblems/src/ProblemsDay_241/LargestSubstringBetweenTwoEqual.java
@@ -0,0 +1,21 @@
+package ProblemsDay_241;
+
+public class LargestSubstringBetweenTwoEqual {
+
+    //https://leetcode.com/problems/largest-substring-between-two-equal-characters/?envType=problem-list-v2&envId=string
+    public int maxLengthBetweenEqualCharacters(String s) {
+        int ans = -1;
+        for (int left = 0; left < s.length(); left++) {
+            for (int right = left + 1; right < s.length(); right++) {
+                if (s.charAt(left) == s.charAt(right)) {
+                    ans = Math.max(ans, right - left - 1);
+                }
+            }
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
+}
