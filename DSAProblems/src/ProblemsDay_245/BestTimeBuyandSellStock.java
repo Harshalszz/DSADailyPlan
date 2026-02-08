2e9a6f33b6bb613dd500f46c73c091587874522e
Harshalszz <477harshz@gmail.com>
2026-02-08 19:05:58 +0530

    BestTimeBuyandSellStock Day245

---
+++ DSAProblems/src/ProblemsDay_245/BestTimeBuyandSellStock.java
@@ -0,0 +1,23 @@
+package ProblemsDay_245;
+
+public class BestTimeBuyandSellStock {
+
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    public int maxProfit(int[] prices) {
+
+        int min = prices[0];
+        int profit = 0;
+
+        for(int i=0; i<prices.length; i++){
+
+            profit = Math.max(profit, prices[i] - min);
+            min = Math.min(min , prices[i]);
+        }
+
+        return profit;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
